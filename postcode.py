89659739d2b8fa5d2c361c172b9440c8692185c5
llsiddharthll <siddharthgreat443@gmail.com>
2024-10-14 02:02:40 +0530

    update

--- postcode.py
+++ postcode.py
@@ -5,10 +5,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import chromedriver_autoinstaller
-
-# Install the ChromeDriver
-chromedriver_autoinstaller.install()
 
 app = Flask(__name__)
 
